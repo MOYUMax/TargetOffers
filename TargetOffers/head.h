24adae08c48cca2a04da5e2134abebcaf4ae6d05
maxwang <webwss@yeah.net>
2017-11-09 19:56:48 +0800

    TGOF first push Lab20171109

---
+++ TargetOffers/head.h
@@ -0,0 +1,13 @@
+#ifndef _HEAD_H
+#define _HEAD_H
+
+#include <iostream>
+#include <string>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+bool Find(int target, vector<vector<int> > array);
+
+#endif
